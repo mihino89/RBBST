c7c1fd7ebafcb37d4ac3263fb1de20eee92a142e
mihino89 <martin.mihalovic98@gmail.com>
2020-03-24 11:24:19 +0100

    add visualisation and comments

--- my_BVS.c
+++ my_BVS.c
@@ -1,21 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define KRED  "\x1B[31m"
+#define KBLU  "\x1B[34m"
+
 #define BLACK 1
 #define RED 0
+
+#define COUNT 10 
 
 
 typedef struct node {
     int val;
     int color;
-    struct node *right, *left, *parent;
+    struct node *right, *left, *parrent;
 } NODE;
+
+
+void print2DUtil(NODE *head, int space) { 
+    // Base case 
+    if (head == NULL) 
+        return; 
+  
+    // Increase distance between levels 
+    space += COUNT; 
+  
+    // Process right child first 
+    print2DUtil(head->right, space); 
+  
+    // Print current node after space 
+    // count 
+    printf("\n"); 
+    for (int i = COUNT; i < space; i++) 
+        printf(" "); 
+        
+    if(head->color == RED){
+        printf("%s%d,%d\n", KRED, head->val, head->color);
+    } else {
+        printf("%s%d\n", KBLU, head->val);
+    }
+  
+    // Process left child 
+    print2DUtil(head->left, space); 
+} 
 
 
 NODE *createNode(int val){
     NODE *node = (NODE *)malloc(sizeof(NODE));
 
-    node->left = node->right = node->parent = NULL;
+    node->left = node->right = node->parrent = NULL;
     node->val = val;
     node->color = RED;
 
@@ -23,59 +56,93 @@ NODE *createNode(int val){
 }
 
 
-void rules_check(){
-    /**
-     * There will be rules check of black red alg.
-    */
+NODE *find_parents_sibling(NODE *current){
+    NODE *parrent, *parrent_of_parrent;
+    parrent = current->parrent;
+    parrent_of_parrent = parrent->parrent;
+
+    if(parrent->val > parrent_of_parrent->val){
+
+    }
 }
 
 
-void insert(NODE *head, int new_value){
+/**
+ * There will be rules check of black red alg.
+*/
+NODE *rules_check(NODE *head, NODE *current){
+    
+    /**
+     * 3. If parent is black
+    */
+    if(current->parrent->color == BLACK){
+        printf("mam rodica BLACK farby, hodnota rodica: %d, moja hodnota: %d\n", current->parrent->val, current->val);
+        return head;
+    }
+
+    printf("mam rodica RED farby, hodnota rodica: %d, moja hodnota: %d\n", current->parrent->val, current->val);
+    return head;
+}
+
+
+NODE *insert(NODE *head, int new_value){
     NODE *current, *parrent;
     current = head;
     parrent = NULL;
 
+    /**
+     * 1. If tree is empty, create new node as root (head) node with black color
+    */
     if(head == NULL){
         head = createNode(new_value);
         head->color = BLACK;
+
+        return head;
     }
 
+    /* Find a leaf for new node with value */
     while (current != NULL){
         parrent = current;
 
-        if(current->val < new_value){
+        if(current->val < new_value)
             current = current->right;
-        }
         else
             current = current->left;       
     }
 
+    /**
+     * 2. If tree is not empty, create new node as leaf node with red (default) color
+    */
     current = createNode(new_value);
 
+    /* Priradenie dietata pre parenta */
     if(parrent->val > new_value){
         parrent->left = current;
     } else {
         parrent->right = current;
     }
 
-    current->parent = parrent;
+    /* Priradenie rodica pre dieta*/
+    current->parrent = parrent;
 
-    if(parrent->color == BLACK){
-        printf("mam rodica BLACK farby %d\n", parrent->val);
+    /* Kontrola pravidiel cierno cervenho stromu */
+    head = rules_check(head, current);
 
-    }
-    else if(parrent->color == RED){
-        printf("mam rodica RED farby\n");
-    }
-
-    // rules_check
+    return head;
 }
 
 
 int main(void) {
-    NODE *head = createNode(50);
-    head->color = BLACK;
+    NODE *head;
 
-    insert(head, 70);
-    insert(head, 80);
+    head = insert(head, 50);
+    head = insert(head, 70);
+    head = insert(head, 75);
+    head = insert(head, 85);
+    head = insert(head, 80);
+    head = insert(head, 30);
+    head = insert(head, 20);
+    head = insert(head, 100);
+
+    print2DUtil(head, 0);
 }
